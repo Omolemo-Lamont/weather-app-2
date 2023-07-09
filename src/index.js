28bebff69fb3a41901f9127f46aa7a01a0a0d0db
Omolemo_Lamont <omolemolamont@gmail.com>
2023-07-09 23:07:19 +0200

    corrected the error of the icon

--- src/index.js
+++ src/index.js
@@ -46,7 +46,7 @@ function displayForecast(response) {
        <img
          src="http://openweathermap.org/img/wn/${
            forecastDay.weather[0].icon
-         }@2.png"
+         }@2x.png"
          alt=""
          w-30
        />
